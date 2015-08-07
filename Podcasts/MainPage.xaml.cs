20c25b4c94b2b7966bfaf97a4d8385ca67469afe
Andrew Gaspar <andrew.gaspar@outlook.com>
2015-08-07 04:55:43 -0700

    It plays the latest Giant Bombcast

---
+++ Podcasts/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Playback;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Podcasts
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private AutoResetEvent backgroundAudioTaskStarted = new AutoResetEvent(false);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadPodcast();
+        }
+
+        public async void LoadPodcast()
+        {
+            var document = await XmlDocument.LoadFromUriAsync(new Uri("http://www.giantbomb.com/podcast-xml/giant-bombcast/"));
+
+            var title = document.SelectNodes("/rss/channel/title").First();
+
+            CurrentPodcastName.Text = title.InnerText;
+
+            var firstItem = document.SelectSingleNode("/rss/channel/item");
+
+            var enclosure = firstItem.SelectSingleNode("enclosure");
+            var urlItem = enclosure.Attributes.GetNamedItem("url").InnerText;
+
+            if (MediaPlayerState.Closed == BackgroundMediaPlayer.Current.CurrentState)
+            {
+                await StartBackgroundAudioTask();
+            }
+
+            BackgroundMediaPlayer.SendMessageToBackground(new ValueSet() {["podcast"] = urlItem });
+        }
+
+        private void AddMediaPlayerEventHandlers()
+        {
+            BackgroundMediaPlayer.Current.CurrentStateChanged += Current_CurrentStateChanged;
+            BackgroundMediaPlayer.MessageReceivedFromBackground += BackgroundMediaPlayer_MessageReceivedFromBackground;
+        }
+
+        private async Task StartBackgroundAudioTask()
+        {
+            AddMediaPlayerEventHandlers();
+
+            var tcs = new TaskCompletionSource<int>();
+            
+            var startResult = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                var result = backgroundAudioTaskStarted.WaitOne(10000);
+                if(result)
+                {
+                    tcs.SetResult(0);
+                }
+                else
+                {
+                    tcs.SetException(new Exception("Background Audio Task didn't start"));
+                }
+            });
+
+            await tcs.Task;
+
+
+        }
+
+        private void BackgroundMediaPlayer_MessageReceivedFromBackground(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            backgroundAudioTaskStarted.Set();
+        }
+
+        private void Current_CurrentStateChanged(MediaPlayer sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+        }
+    }
+}
---
+++ PodcastsBackgroundAudio/BackgroundAudioTask.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+
+namespace PodcastsBackground
+{
+    public sealed class BackgroundAudioTask : IBackgroundTask
+    {
+        private SystemMediaTransportControls mediaTransportControls;
+        private BackgroundTaskDeferral deferral;
+        private ManualResetEvent backgroundTaskStarted = new ManualResetEvent(false);
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            mediaTransportControls = BackgroundMediaPlayer.Current.SystemMediaTransportControls;
+            mediaTransportControls.ButtonPressed += MediaButtonPressed;
+            mediaTransportControls.PropertyChanged += MediaPropertyChanged;
+            mediaTransportControls.IsEnabled = true;
+            mediaTransportControls.IsPauseEnabled = true;
+            mediaTransportControls.IsPlayEnabled = true;
+            mediaTransportControls.IsNextEnabled = true;
+            mediaTransportControls.IsPreviousEnabled = true;
+
+            BackgroundMediaPlayer.Current.CurrentStateChanged += MediaPlayerStateChanged;
+            BackgroundMediaPlayer.MessageReceivedFromForeground += OnMessageReceived;
+
+
+            BackgroundMediaPlayer.SendMessageToForeground(new ValueSet());
+
+            deferral = taskInstance.GetDeferral();
+
+            backgroundTaskStarted.Set();
+
+            taskInstance.Task.Completed += TaskCompleted;
+            taskInstance.Canceled += OnCanceled;
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void TaskCompleted(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            deferral.Complete();
+        }
+
+        private void OnMessageReceived(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            var url = e.Data["podcast"] as string;
+
+            var source = MediaSource.CreateFromUri(new Uri(url));
+            BackgroundMediaPlayer.Current.Source = source;
+        }
+
+        private void MediaPlayerStateChanged(MediaPlayer sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void MediaPropertyChanged(SystemMediaTransportControls sender, SystemMediaTransportControlsPropertyChangedEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void MediaButtonPressed(SystemMediaTransportControls sender, SystemMediaTransportControlsButtonPressedEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
