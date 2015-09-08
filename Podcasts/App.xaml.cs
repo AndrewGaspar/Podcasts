2576bdd0e996df22da3dd5aed5e51c34471bac4a
Andrew Gaspar <andrew.gaspar@outlook.com>
2015-09-07 20:43:56 -0700

    Add automatically updating brushes for accent colors

--- Podcasts/App.xaml.cs
+++ Podcasts/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Podcasts.Transport;
 using Podcasts.ViewModels;
 using Windows.ApplicationModel;
@@ -12,6 +13,87 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Podcasts
 {
+
+    public class PresentationSettings : DependencyObject
+    {
+        public class AccentColorValues
+        {
+            public Color AccentColor { get; set; }
+            public Color AccentColorDark1 { get; set; }
+            public Color AccentColorDark2 { get; set; }
+            public Color AccentColorDark3 { get; set; }
+            public Color AccentColorLight1 { get; set; }
+            public Color AccentColorLight2 { get; set; }
+            public Color AccentColorLight3 { get; set; }
+        }
+
+        private UISettings UISettings = new UISettings();
+
+        public PresentationSettings()
+        {
+            if (!DesignMode.DesignModeEnabled)
+            {
+                AccentColors = new AccentColorValues()
+                {
+                    AccentColor = GetDefaultColor("DefaultAccentColor"),
+                    AccentColorDark1 = GetDefaultColor("DefaultAccentColorDark1"),
+                    AccentColorDark2 = GetDefaultColor("DefaultAccentColorDark2"),
+                    AccentColorDark3 = GetDefaultColor("DefaultAccentColorDark3"),
+                    AccentColorLight1 = GetDefaultColor("DefaultAccentColorLight1"),
+                    AccentColorLight2 = GetDefaultColor("DefaultAccentColorLight2"),
+                    AccentColorLight3 = GetDefaultColor("DefaultAccentColorLight3"),
+                };
+            }
+
+            try
+            {
+                UISettings.ColorValuesChanged += UISettings_ColorValuesChanged;
+
+                UpdateColors();
+            }
+            catch(InvalidCastException)
+            {
+                UISettings = null;
+            }
+        }
+
+        private void UISettings_ColorValuesChanged(UISettings sender, object args)
+        {
+            UpdateColors();
+        }
+
+        private static Color GetDefaultColor(string name)
+        {
+            return (Color)Application.Current.Resources[name];
+        }
+
+        public AccentColorValues AccentColors
+        {
+            get { return (AccentColorValues)GetValue(AccentColorsProperty); }
+            set { SetValue(AccentColorsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AccentColors.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AccentColorsProperty =
+            DependencyProperty.Register("AccentColors", typeof(AccentColorValues), typeof(PresentationSettings), new PropertyMetadata(0));
+        
+        public Color GetColor(UIColorType type) => UISettings.GetColorValue(type);
+
+        public void UpdateColors()
+        {
+            AccentColors = new AccentColorValues
+            {
+                AccentColor = GetColor(UIColorType.Accent),
+                AccentColorDark1 = GetColor(UIColorType.AccentDark1),
+                AccentColorDark2 = GetColor(UIColorType.AccentDark2),
+                AccentColorDark3 = GetColor(UIColorType.AccentDark3),
+                AccentColorLight1 = GetColor(UIColorType.AccentLight1),
+                AccentColorLight2 = GetColor(UIColorType.AccentLight2),
+                AccentColorLight3 = GetColor(UIColorType.AccentLight3),
+            };
+        }
+    }
+
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
@@ -19,8 +101,10 @@ namespace Podcasts
     {
         public ForeroundMessageTransport MessageService = new ForeroundMessageTransport();
 
-        public AppViewModel AppViewModel = new AppViewModel();
+        private AppViewModel AppViewModel = new AppViewModel();
 
+        public AppViewModel ViewModel => AppViewModel;
+        
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -33,60 +117,43 @@ namespace Podcasts
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
+        
 
         /// <summary>
         /// Invoked when the application is launched normally by the end user.  Other entry points
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected async override void OnLaunched(LaunchActivatedEventArgs e)
+        /// <param name="args">Details about the launch request and process.</param>
+        protected async override void OnLaunched(LaunchActivatedEventArgs args)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
+            Chrome mainPage = Window.Current.Content as Chrome;
             
             await AppViewModel.InitializeAsync();
-
+            
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
-            if (rootFrame == null)
+            if (mainPage == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+                // Create the page chrome to act as the navigation context and navigate to the first page
+                mainPage = new Chrome();
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: Load state from previously suspended application
                 }
 
                 // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
+                Window.Current.Content = mainPage;
             }
 
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
-            }
+            mainPage.AppLaunched(args);
 
             MessageService.Start();
             
             // Ensure the current window is active
             Window.Current.Activate();
         }
-
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
+        
         /// <summary>
         /// Invoked when application execution is being suspended.  Application state is saved
         /// without knowing whether the application will be terminated or resumed with the contents
--- Podcasts/Chrome.xaml.cs
+++ Podcasts/Chrome.xaml.cs
@@ -10,6 +10,7 @@ using Podcasts.Dom;
 using Podcasts.Messages;
 using Podcasts.Models;
 using Podcasts.ViewModels;
+using Windows.ApplicationModel.Activation;
 using Windows.Data.Xml.Dom;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -35,56 +36,43 @@ namespace Podcasts
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Chrome : Page
     {
         public App App => (App)Application.Current;
 
-        public AppViewModel ViewModel => App.AppViewModel;
+        public AppViewModel ViewModel => App.ViewModel;
 
-        public MainPage()
+        public bool NavigateTo(Type destination, object arguments) => RootFrame.Navigate(destination, arguments);
+
+        public Chrome()
         {
             this.InitializeComponent();
             this.DataContext = this;
+
+            this.RootFrame.NavigationFailed += this.OnNavigationFailed;
         }
 
-        public class PresentationBindings : PropertyChangeBase
+        public void AppLaunched(LaunchActivatedEventArgs e)
         {
-            private UISettings UISettings { get; } = new UISettings();
-
-            public PresentationBindings()
+            if (this.RootFrame.Content == null)
             {
-                UpdateAppBarBrush();
-                
-                UISettings.ColorValuesChanged += UISettings_ColorValuesChanged;
-            }
-
-            private void UISettings_ColorValuesChanged(UISettings sender, object args)
-            {
-                UpdateAppBarBrush();
-            }
-
-            private void UpdateAppBarBrush()
-            {
-                AppBarBrush = new SolidColorBrush(UISettings.GetColorValue(UIColorType.AccentDark1));
-            }
-            
-            private Brush _appBarBrush;
-            public Brush AppBarBrush
-            {
-                get
-                {
-                    return _appBarBrush;
-                }
-                private set
-                {
-                    _appBarBrush = value;
-                    NotifyPropertyChanged();
-                }
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                this.RootFrame.Navigate(typeof(Views.PodcastsListPage), e.Arguments);
             }
         }
 
-        public PresentationBindings Presentation { get; } = new PresentationBindings();
-
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
