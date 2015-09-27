cdaaaa527cc51fd35c7f8153bdc7a8924015511c
Andrew Gaspar <andrew.gaspar@outlook.com>
2015-09-27 16:48:03 -0700

    Changed how the duration of podcasts are displayed.

---
+++ Podcasts.Shared/Converters/PodcastDurationConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Podcasts.Converters
+{
+    public class PodcastDurationConverter : TypedConverter<TimeSpan, string>
+    {
+        public override string Convert(TimeSpan value, object parameter, string language)
+        {
+            if (value.TotalHours >= 1.0)
+            {
+                return value.ToString(@"h\:mm\:ss");
+            }
+            else
+            {
+                return value.ToString(@"m\:ss");
+            }
+        }
+
+        public override TimeSpan ConvertBack(string value, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
